8ae4cb9112fb6faa4416c82545f50a3dff75863f
yycer <15900617310@163.com>
2020-02-16 21:49:52 +0800

    Play with bean.

---
+++ src/main/java/com/frankie/demo/beans/MyTestBean.java
@@ -0,0 +1,13 @@
+package com.frankie.demo.beans;
+
+import lombok.Getter;
+
+/**
+ * @author: Yao Frankie
+ * @date: 2020/2/16 21:20
+ */
+@Getter
+public class MyTestBean {
+
+    private String test = "test";
+}
--- src/test/java/com/frankie/demo/DemoApplicationTests.java
+++ src/test/java/com/frankie/demo/DemoApplicationTests.java
@@ -1,7 +1,11 @@
 package com.frankie.demo;
 
+import com.frankie.demo.beans.MyTestBean;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -9,6 +13,14 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("spring");
+    }
+
+    @Test
+    public void beanTest(){
+        ClassPathResource resource = new ClassPathResource("/beanFactoryTest.xml");
+        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(resource);
+        MyTestBean myTestBean = (MyTestBean) xmlBeanFactory.getBean("myTestBean");
+        Assert.assertEquals(myTestBean.getTest(), "test");
     }
 
 }
